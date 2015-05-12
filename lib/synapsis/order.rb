5d9b2cdfd96a3c495f42c387afa809e24fdc694d
Daryll Santos <daryll.santos@gmail.com>
2015-05-12 16:44:01 +0800

    Refactor Order to return Response and raise Error.

--- lib/synapsis/order.rb
+++ lib/synapsis/order.rb
@@ -1,35 +1,17 @@
 class Synapsis::Order < Synapsis::APIResource
   include Synapsis::Utilities
-
-  attr_accessor :balance_verified, :success, :account_type, :amount, :date, :date_settled, :discount, :facilitator_fee, :fee, :id, :is_buyer, :note, :resource_uri, :seller, :status, :status_uri, :ticket_number, :tip, :total
-
-  ORDER_PARAMS = ['account_type', 'amount', 'date', 'date_settled', 'discount', 'facilitator_fee', 'fee', 'id', 'is_buyer', 'note', 'resource_uri', 'seller', 'status', 'status_uri', 'ticket_number', 'tip', 'total']
-
-  SYNAPSE_FEE = 0.10
+  extend Synapsis::APIOperations::Create
 
   def self.add(params)
     response = create_request(params)
+    return_response(response)
+  end
 
-    parsed_response = JSON.parse(response.body)
-
-    if parsed_response['success']
-      return new(parsed_response)
+  def self.synapse_fee(transaction_amount)
+    if transaction_amount > 10
+      0.25
     else
-      raise Synapsis::Error, parsed_response['reason']
-    end
-  end
-
-  def self.synapse_fee
-    SYNAPSE_FEE
-  end
-
-  def initialize(params)
-    ['success', 'balance_verified'].each do |k|
-      send("#{k}=", params[k])
-    end
-
-    ORDER_PARAMS.each do |k|
-      send("#{k}=", params['order'][k])
+      0.1
     end
   end
 end
--- spec/synapsis/order_spec.rb
+++ spec/synapsis/order_spec.rb
@@ -2,50 +2,58 @@ require 'spec_helper'
 
 RSpec.describe Synapsis::Order do
   context '#add' do
+    let!(:delta) { 0.001 }
+    let!(:buyer_consumer_key) { '3bdb5790692d06983d8cb0feb40365886631e52d' }
+    let!(:seller_consumer_key) { '325ea5c0c3a7927280c54ed3ad310c02b45129d8' }
+    let!(:order_params) {{
+      amount: 1,
+      oauth_consumer_key: buyer_consumer_key,
+      seller_id: 3437
+    }}
+
     context 'happy path' do
       it 'constructs the correct Order object' do
-        order_params = {
-          amount: 1,
-          oauth_consumer_key: '3bdb5790692d06983d8cb0feb40365886631e52d',
-          seller_id: 3437
-        }
-
         order = Synapsis::Order.add(order_params)
 
-        (order.instance_variables - [:@status_uri]).each do |x|
-          expect(order.send(x.to_s.gsub('@', ''))).not_to be_nil
+        ORDER_PARAMS = ['account_type', 'amount', 'date', 'date_settled', 'discount', 'facilitator_fee', 'fee', 'id', 'is_buyer', 'note', 'resource_uri', 'seller', 'status', 'status_uri', 'ticket_number', 'tip', 'total']
+
+        (ORDER_PARAMS - ['status_uri']).each do |x|
+          expect(order.order.send(x)).not_to be_nil
         end
       end
 
       it 'subtracts the money from the consumer\'s account and adds to the seller\'s account' do
-        buyer_account_balance = Synapsis::User.view('3bdb5790692d06983d8cb0feb40365886631e52d').balance
-        seller_account_balance = Synapsis::User.view('325ea5c0c3a7927280c54ed3ad310c02b45129d8').balance
-
-        order_params = {
-          amount: 10,
-          oauth_consumer_key: '3bdb5790692d06983d8cb0feb40365886631e52d',
-          seller_id: 3437
-        }
+        buyer_account_balance = Synapsis::User.view(buyer_consumer_key).user.balance
+        seller_account_balance = Synapsis::User.view(seller_consumer_key).user.balance
 
         order = Synapsis::Order.add(order_params)
 
-        new_buyer_account_balance = Synapsis::User.view('3bdb5790692d06983d8cb0feb40365886631e52d').balance
-        new_seller_account_balance = Synapsis::User.view('325ea5c0c3a7927280c54ed3ad310c02b45129d8').balance
+        new_buyer_account_balance = Synapsis::User.view(buyer_consumer_key).user.balance
+        new_seller_account_balance = Synapsis::User.view(seller_consumer_key).user.balance
 
-        expect(new_buyer_account_balance).to eq buyer_account_balance - order_params[:amount]
-        expect(new_seller_account_balance).to eq seller_account_balance + order_params[:amount] - Synapsis::Order.synapse_fee
+        expect(new_buyer_account_balance).to be_within(delta).of(buyer_account_balance - order_params[:amount])
+        expect(new_seller_account_balance).to be_within(seller_account_balance + order_params[:amount] - Synapsis::Order.synapse_fee(order_params[:amount]))
+      end
+
+      it 'subtracts the money from the consumer\'s account and adds to the seller\'s account, with a charge of ' do
+        buyer_account_balance = Synapsis::User.view(buyer_consumer_key).user.balance
+        seller_account_balance = Synapsis::User.view(seller_consumer_key).user.balance
+
+        order = Synapsis::Order.add(order_params.merge(amount: 10.1))
+
+        new_buyer_account_balance = Synapsis::User.view(buyer_consumer_key).user.balance
+        new_seller_account_balance = Synapsis::User.view(seller_consumer_key).user.balance
+
+        expect(new_buyer_account_balance).to be_within(delta).of(buyer_account_balance - 10.1)
+        expect(new_seller_account_balance).to be_within(delta).of(seller_account_balance + 10.1- 0.25)
       end
     end
 
     context 'no amount specified' do
       it 'raises a Synapsis::Error' do
-        order_params = {
-          amount: 0,
-          oauth_consumer_key: '3bdb5790692d06983d8cb0feb40365886631e52d',
-          seller_id: 3437
-        }
-
-        expect{ Synapsis::Order.add(order_params) }.to raise_error(Synapsis::Error).with_message('Missing amount')
+        expect{ Synapsis::Order.add(order_params.merge(oauth_consumer_key: 'WRONG!')) }.to raise_error(Synapsis::Error).with_message('Error in OAuth Authentication.')
+        expect{ Synapsis::Order.add(order_params.merge(amount: 0)) }.to raise_error(Synapsis::Error).with_message('Missing amount')
+        expect{ Synapsis::Order.add(order_params.merge(seller_id: 0)) }.to raise_error(Synapsis::Error).with_message('Missing seller_id')
       end
     end
   end
