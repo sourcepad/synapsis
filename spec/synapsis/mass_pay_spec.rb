4725e33e6d47ab7fa05d879bd63473dc5d97c1d5
Daryll Santos <daryll.santos@gmail.com>
2015-05-16 00:44:43 +0800

    Added test to MassPay.add using Card parameters.

--- spec/synapsis/mass_pay_spec.rb
+++ spec/synapsis/mass_pay_spec.rb
@@ -47,6 +47,43 @@ RSpec.describe Synapsis::MassPay do
       end
     end
   end
+
+  describe '.add (via cards)' do
+    # These tests might fail if the guy runs out of money
+    context 'happy path' do
+      let!(:delta) { 0.001 }
+      let!(:users_consumer_key) { 'dcd234d9d9fb55ad9711c4c41e254868ef3768d4' }
+      let!(:mass_pay_hash) {{
+        amount: 1,
+        card_id: 359,
+        trans_type: 0
+      }}
+      let!(:mass_pay_hash2) {{
+        amount: 1,
+        card_id: 360,
+        trans_type: 0
+      }}
+
+      it 'returns an array of MassPay objects' do
+        mass_pay_response = Synapsis::MassPay.add(
+          mass_pays: [mass_pay_hash, mass_pay_hash2],
+          oauth_consumer_key: users_consumer_key)
+
+        expect(mass_pay_response).to respond_to(:mass_pays)
+        expect(mass_pay_response.mass_pays.count).to eq 2
+        expect(mass_pay_response).to respond_to(:success)
+      end
+
+      it 'deducts the user\'s account by the total amount plus 0.1 per mass pay' do
+        users_balance = Synapsis::User.view(users_consumer_key).user.balance
+
+        Synapsis::MassPay.add(
+          mass_pays: [mass_pay_hash, mass_pay_hash2],
+          oauth_consumer_key: users_consumer_key)
+
+        users_balance_after = Synapsis::User.view(users_consumer_key).user.balance
+        expect(users_balance_after).to be_within(delta).of(users_balance - mass_pay_hash[:amount] - mass_pay_hash2[:amount] - (2 * Synapsis::MassPay.cost_per_mass_pay))
+      end
+    end
+  end
 end
-
-
